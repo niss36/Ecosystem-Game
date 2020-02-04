fef03460f167460f057dbd9199e2da3bed2525fe
niss36 <nissim.chekroun@gmail.com>
2020-02-04 12:00:34 +0000

    Basic components

---
+++ frontend/src/components/Decisions/Decisions.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Decisions extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>Decisions</p>
+            </div>
+        );
+    }
+}
---
+++ frontend/src/components/EcosystemData/EcosystemData.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class EcosystemData extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>EcosystemData</p>
+            </div>
+        );
+    }
+}
