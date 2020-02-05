f6995c3db49af53491453e49aee883ec3cf98fc7
niss36 <nissim.chekroun@gmail.com>
2020-02-05 14:24:33 +0000

    Map Settings button + pop-up menu

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,15 +1,63 @@
 import React from "react";
 
 import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Settings from "@material-ui/icons/Settings";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import "./Map.css";
 
 export default class Map extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            settingsPopupAnchor: null,
+        };
+
+        this.openSettingsPopup = this.openSettingsPopup.bind(this);
+        this.closeSettingsPopup = this.closeSettingsPopup.bind(this);
+    }
+
+    openSettingsPopup(event) {
+        this.setState({settingsPopupAnchor: event.currentTarget});
+    }
+
+    closeSettingsPopup() {
+        this.setState({settingsPopupAnchor: null});
+    }
+
     render() {
         return (
             <Grid item xs={12} md={8} className="Map-root">
                 <div className="panel">
+                    <div className="Map-settings">
+                        <IconButton aria-label="map settings" onClick={this.openSettingsPopup}>
+                            <Settings/>
+                        </IconButton>
+                        <Menu
+                            id="settings-menu"
+                            anchorEl={this.state.settingsPopupAnchor}
+                            getContentAnchorEl={null}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            open={Boolean(this.state.settingsPopupAnchor)}
+                            onClose={this.closeSettingsPopup}
+                        >
+                            <MenuItem onClick={this.closeSettingsPopup}>Setting 1</MenuItem>
+                            <MenuItem onClick={this.closeSettingsPopup}>Setting 2</MenuItem>
+                            <MenuItem onClick={this.closeSettingsPopup}>Setting 3</MenuItem>
+                        </Menu>
+                    </div>
                     <p>Map</p>
                 </div>
             </Grid>
