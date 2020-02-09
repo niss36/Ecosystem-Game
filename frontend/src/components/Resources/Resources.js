1864bc8471891ea6980c757a682fc6f3a8b89cf5
Alex Vanlint <m00113@reading-school.co.uk>
2020-02-09 15:10:25 +0000

    Added variables to resources

--- frontend/src/components/Resources/Resources.js
+++ frontend/src/components/Resources/Resources.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 import "./Resources.css";
-
 export default class Resources extends React.Component {
-
+    money = 0;
+    food = 0;
+    population = 0;
+    happiness = 0;
+    electricity = 0;
+    wood = 0;
     render() {
         return (
             <div className="Resources-root">
                 <h3>Resources</h3>
 
                 <ul>
-                    <li>Money</li>
-                    <li>Food</li>
-                    <li>Population</li>
-                    <li>Happiness</li>
-                    <li>Electricity</li>
-                    <li>Wood</li>
+                    <li>Money: {this.money}</li>
+                    <li>Food: {this.food}</li>
+                    <li>Population: {this.population}</li>
+                    <li>Happiness: {this.happiness}</li>
+                    <li>Electricity: {this.electricity}</li>
+                    <li>Wood: {this.wood}</li>
                 </ul>
             </div>
         );
