5b0568e70d51f91d49bdee88734d895aa1d13200
niss36 <nissim.chekroun@gmail.com>
2020-02-13 20:51:44 +0000

    Forgot to remove the Energy tab

--- frontend/src/components/Decisions/Decisions.js
+++ frontend/src/components/Decisions/Decisions.js
@@ -47,7 +47,7 @@ class Decisions extends React.Component {
 
         const {onNextTurn, onDecisionStuff} = this.props;
 
-        const tabs = ["Food", "Forestry", "Population", "Energy"];
+        const tabs = ["Food", "Forestry", "Population"];
 
         return (
             <div className="Decisions-root panel">
