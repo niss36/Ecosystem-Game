6c666e1cc7939401fbc635105e57387f11682c51
DCSmythe <DCSmythe@aol.com>
2020-02-13 13:05:39 +0000

    Added a data and changes state to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,12 +11,34 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
+        let dataCells = new Array(16 * 16);
+
+        let desiredChanges = new Array(16 * 16);
+
+        for(let i = 0; i < dataCells.length; i++){
+            dataCells[i] = {};
+            desiredChanges[i] = {fishing: [], hunting: []};
+        }
+
         this.state = {
-            mapargs : {selectionMode : false }
+            mapargs : {selectionMode : false },
+            dataCells: dataCells,
+            desiredChanges: desiredChanges,
         };
 
         this.decisionsCall = this.decisionsCall.bind(this);
         this.nextTurn = this.nextTurn.bind(this);
+        this.submitChange = this.submitChange.bind(this);
+    }
+
+    submitChange(i, key, value){
+        let selectedCell = this.state.desiredChanges[i];
+        if(key === "fishing"){
+            selectedCell.fishing.push(value);
+        }
+        else if(key === "hunting"){
+            selectedCell.hunting.push(value);
+        }
     }
 
     decisionsCall(name) {
