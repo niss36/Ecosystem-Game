157eedf1e47656a33debf98df1ae07ed0e1cfb28
nuomicii <yj309@cam.ac.uk>
2020-02-18 19:39:29 +0800

    added icon to BuildingPane

--- frontend/src/components/Decisions/Buildings.js
+++ frontend/src/components/Decisions/Buildings.js
@@ -7,19 +7,19 @@ export const buildings = {
         name: "Animal Farm",
         description: "Rear animals to produce food.",
         costs: [
-            {resource: WOOD, amount: 200},
-            {resource: MONEY, amount: 100},
+            {resource: WOOD,amount: 200,icon:"/logs.svg",},
+            {resource: MONEY, amount: 100,icon:"/coin.svg"},
         ],
         effects: [
-            {resource: FOOD, income: 100},
+            {resource: FOOD, income: 100,icon:"/meat.svg"},
         ],
     },
     fishingBoat: {
         name: "Fishing Boat",
         description: "A boat to catch fish in the ocean.",
         costs: [
-            {resource: WOOD, amount: 200},
-            {resource: MONEY, amount: 100},
+            {resource: WOOD, amount: 200,icon:"/logs.svg"},
+            {resource: MONEY, amount: 100,icon:"/coin.svg"},
         ],
         effects: [],
     },
@@ -27,11 +27,11 @@ export const buildings = {
         name: "Hunting Shack",
         description: "Hire hunters to harvest wild animals.",
         costs: [
-            {resource: WOOD, amount: 100},
-            {resource: MONEY, amount: 400},
+            {resource: WOOD, amount: 100,icon:"/logs.svg"},
+            {resource: MONEY, amount: 400,icon:"/coin.svg"},
         ],
         effects: [
-            {resource: FOOD, income: 100},
+            {resource: FOOD, income: 100,icon:"/meat.svg"},
         ],
     },
     cheapLumberMill: {
