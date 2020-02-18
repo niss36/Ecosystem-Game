b71ee1ea3367876c0c0db8c6bb28f01bb35f1897
marcus800 <marcussamuel80@gmail.com>
2020-02-18 12:52:28 +0000

    Added a map, can change by clicking on (temp)

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -10,7 +10,7 @@ import Cell from "./Cell";
 
 import "./Map.css";
 
-const size = 16;
+const size = 20;
 
 class Map extends React.Component {
     constructor(props) {
@@ -18,6 +18,8 @@ class Map extends React.Component {
 
         this.state = {
             selectedIndexs: [],
+            island :[210,211,231,271,251,270,269,250,230,247,229,249,209,227,189,188,190,192,228,232,191,208,212,206,252,272,246,187,245,207,226,168,170,169,171,293,292,268,266,248,290,291,287,310,307,289,306,308,267,309,286,288,311,312,233,213,173,153,174,175,176,196,217,216,236,172,152,193,154,235,197,177,329,330,331,326,265,305,225,186,149,151,150,131,132,304,325,166,148,108,128,110,109,130,89,88,90,129,111,107,127,87,106,86,105,125,124,165,163,185,205,224,184,164,204]
+            //maybe make set or sort idk...
         };
 
         this.cellMouseMove = this.cellMouseMove.bind(this);
@@ -35,8 +37,23 @@ class Map extends React.Component {
         }
     }
     cellMouseclick(i) {
+        // temp code to change map
+        if (!this.state.island.includes(i)) {
+            this.setState({island: [...this.state.island, i]});
+        } else {
+            this.setState({island: this.state.island.filter( item => item !== i)
+                    });
+        }
+        let log = "[";
+        for (const x of this.state.island) {
+            log+= x+","
+        } console.log(log);
+
+
+
         if (!this.props.mapargs.selectionMode) {
             this.setState({selectedIndexs :[i]});
+
         } else {
             //TODO when app.js cells is done
             //call fun in app.js
@@ -49,19 +66,21 @@ class Map extends React.Component {
 
     render() {
 
-        const {selectedIndexs } = this.state;
+        const {selectedIndexs,island } = this.state;
 
         let cells = new Array(size * size);
         for (let i = 0; i < cells.length; i++) {
-            if (this.props.mapargs.selectionMode && selectedIndexs.includes(i)) {
+            if (this.props.mapargs.selectionMode && selectedIndexs.includes(i)&& !island.includes(i)) {
 
                 cells[i] = (<Cell cellMouseMove={this.cellMouseMove}
                                   cellMouseclick={this.cellMouseclick}
-                                  selected={true} i={i} key={i}/>);
+                                  selected={true} i={i}
+                                  island={this.state.island} />);
             } else {
                 cells[i] = (<Cell cellMouseMove={this.cellMouseMove}
                                   cellMouseclick={this.cellMouseclick}
-                                  selected={false} i={i} key={i}/>);
+                                  selected={false} i={i}
+                                  island={this.state.island} />);
             }
         }
 
