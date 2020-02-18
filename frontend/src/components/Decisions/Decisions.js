826ee1f8b8e935d28277d14c7891ab051fbd081c
nuomicii <yj309@cam.ac.uk>
2020-02-18 20:53:27 +0800

    scrollbar for hunting

--- frontend/src/components/Decisions/Decisions.js
+++ frontend/src/components/Decisions/Decisions.js
@@ -34,16 +34,21 @@ class Decisions extends React.Component {
 
         this.state = {
             fishBoatEffort: 50,
+            HuntingEffort:100,
             selectedAnimal: 'none',
             fishList: this.props.fishList,
             animalList: this.props.animalList,
         };
 
         this.onFishBoatEffortChange = this.onFishBoatEffortChange.bind(this);
+        this.onHuntEffortChange=this.onHuntEffortChange.bind(this);
     }
 
     onFishBoatEffortChange(e, v) {
         this.setState({fishBoatEffort: v});
+    }
+    onHuntEffortChange(e, v) {
+        this.setState({HuntingEffort: v});
     }
 
     render() {
@@ -68,7 +73,12 @@ class Decisions extends React.Component {
                             <MuiSlider value={this.state.fishBoatEffort} onChange={this.onFishBoatEffortChange} aria-labelledby="fish-effort-slider"/>
                         </MakeBuildingPane>
                         {/*Hunting*/}
-                        <MakeBuildingPane id="huntingShack"/>
+                        <MakeBuildingPane id="huntingShack" onBuy={() => onDecisionStuff("animal")} extraEffects={[{resource: "food", income: this.state.HuntingEffort,icon:"/meat.svg"}]}>
+                            <div style={{textAlign: "center"}} id="hunt-effort-slider">
+                                Effort
+                            </div>
+                            <MuiSlider value={this.state.HuntingEffort} onChange={this.onHuntEffortChange} aria-labelledby="hunt-effort-slider"/>
+                        </MakeBuildingPane>
                     </div>
 
                     {/*Forestry*/}
